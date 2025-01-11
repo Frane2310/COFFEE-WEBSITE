0fd9c8fe452a94674c63a39be33baeb7b53fc454
Frane2310 <franebucic2@gmail.com>
2025-01-11 18:44:24 +0100

    Footer / finishing details

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+
 menuOpenButton.addEventListener("click",()=>{
     document.body.classList.toggle("show-mobile-menu");
 })
+/*Close menu when the close button is clicked*/
 menuCloseButton.addEventListener("click",()=> menuOpenButton.click());
+/*Close menu when the nav link is clicked*/
+navLinks.forEach(link=>{link.addEventListener("click", ()=> menuOpenButton.click());})
+
 //Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
     loop: true,
